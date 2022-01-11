ae0b5f229f191971a4ffb0efb4a0c47fea1da0c0
RisinaR <rkarisina@gmail.com>
2022-01-11 12:53:51 +0530

    Home Page Completed

---
+++ src/data.js
@@ -0,0 +1,97 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://freepngimg.com/thumb/jewellery/5-2-jewellery-free-download-png.png",
+    title: "SUMMER SALE",
+    description:
+      "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://freepngimg.com/thumb/jewellery/6-2-jewellery-free-png-image.png",
+    title: "GOLD COLLECTION",
+    description:
+      "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://freepngimg.com/thumb/jewellery/8-2-jewellery-png-clipart.png",
+    title: "ROYAL COLLECTION",
+    description:
+      "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://www.pngkey.com/png/detail/13-130907_diamond-necklace-pictures-imagebasket-net-3-necklace-png.png",
+    title: "NECKLACE",
+  },
+  {
+    id: 2,
+    img: "https://www.pngkey.com/png/detail/398-3986540_14k-white-gold-engagement-ring-pr0187ecq-44w-pre.png",
+    title: "RING",
+  },
+  {
+    id: 3,
+    img: "https://www.pngkey.com/png/detail/366-3667206_square-emerald-cut-diamond-tennis-bracelet-diamond-tennis.png",
+    title: "BRACELET",
+  },
+  {
+    id: 4,
+    img: "https://www.pngkey.com/png/detail/166-1664950_the-browns-journey-heart-diamond-pendant-heart-pendant.png",
+    title: "PENDANT",
+  },
+  {
+    id: 5,
+    img: "https://www.pngkey.com/png/detail/121-1218245_sliced-diamond-earrings-earring.png",
+    title: "EARRINGS",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://freepngimg.com/thumb/jewellery/7-2-jewellery-picture.png",
+  },
+  {
+    id: 2,
+    img: "https://freepngimg.com/thumb/jewellery/1-2-jewellery-png-picture.png",
+  },
+  {
+    id: 3,
+    img: "https://purepng.com/public/uploads/large/purepng.com-silver-ringjewelryjewellerybroochesringsnecklacesearringsearringsornamentsflowersdiamondsilverring-1701528881414tgi3w.png",
+  },
+  {
+    id: 4,
+    img: "https://purepng.com/public/uploads/large/purepng.com-gold-ringjewelryjewellerydiamondgold-ring-1701528882730cojzn.png",
+  },
+  {
+    id: 5,
+    img: "https://purepng.com/public/uploads/large/purepng.com-jewelryjewelryjewellerydiamondlarge-17015288839804a1fe.png",
+  },
+  {
+    id: 6,
+    img: "https://purepng.com/public/uploads/large/purepng.com-ahana-jewelryjewelryjewellerydiamondlarge-1701528883970a5lvx.png",
+  },
+  {
+    id: 7,
+    img: "https://purepng.com/public/uploads/large/purepng.com-pendantjewelryjewellerydiamondpendantchain-1701528883195mihf7.png",
+  },
+  {
+    id: 8,
+    img: "https://purepng.com/public/uploads/large/love-pendant-ych.png",
+  },
+  {
+    id: 9,
+    img: "https://freepngimg.com/thumb/accessories/170950-moonstone-jewellery-png-image-high-quality.png",
+  },
+  {
+    id: 10,
+    img: "https://purepng.com/public/uploads/large/purepng.com-cinna-earringjewelryjewellerydiamondcinnaearring-1701528883611bkje4.png",
+  },
+];
